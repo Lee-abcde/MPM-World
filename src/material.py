d83a643125f40612e1a8fde43ebf648694c7dde1
Lee-abcde <ljy1340691470@gmail.com>
2023-05-22 10:52:23 +0800

    (dev) 添加2D旋度的计算

--- src/material.py
+++ src/material.py
@@ -30,6 +30,17 @@ def Curl_cal(pos, S_grid_v):
     return curl
 
 @ti.func
+def Curl_cal2D(pos, S_grid_v):
+    vy_right = S_grid_v[[pos[0] + 1, pos[1]]][1]
+    vy_left = S_grid_v[[pos[0] - 1, pos[1]]][1]
+
+    vx_up = S_grid_v[[pos[0], pos[1] + 1]][0]
+    vx_down = S_grid_v[[pos[0], pos[1] - 1]][0]
+
+    dx = 1.
+    curl = (vy_right - vy_left - vx_up + vx_down) / (2. * dx)
+    return curl
+@ti.func
 def CurlGrad_cal(pos, S_grid_c):
     s_right = tm.length(S_grid_c[[pos[0] + 1, pos[1], pos[2]]])
     s_left = tm.length(S_grid_c[[pos[0] - 1, pos[1], pos[2]]])
@@ -57,3 +68,22 @@ def Vorticity_cal(pos, S_grid_c):
         vc_force = vorticitymult * dx * tm.cross(gradcurl / gradcurllenth, curl)
 
     return vc_force
+
+@ti.func
+def Vorticity_cal2D(pos, S_grid_c):
+    dx = 1.0
+    vorticitymult = 1.0
+
+    curl = ti.Vector([0., 0., S_grid_c[pos]])
+    s_right = S_grid_c[[pos[0] + 1, pos[1]]]
+    s_left = S_grid_c[[pos[0] - 1, pos[1]]]
+
+    s_up = S_grid_c[[pos[0], pos[1] + 1]]
+    s_down = S_grid_c[[pos[0], pos[1] - 1]]
+    gradcurl = ti.Vector([s_right - s_left, s_up - s_down, 0]) / (2.0 * dx)
+    vc_force = ti.Vector([0., 0., 0])
+    gradcurllenth = tm.length(gradcurl)
+    if gradcurllenth > 1e-5:
+        vc_force = vorticitymult * dx * tm.cross(gradcurl / gradcurllenth, curl)
+
+    return ti.Vector([vc_force[0],vc_force[1]])
